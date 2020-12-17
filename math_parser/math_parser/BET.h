d6dfc0ce6826702b49d1f3e28555ca790d28ffc5
d.prysenko <prototip5250@gmail.com>
2020-12-18 01:18:38 +0300

    tests and processing minus before paranthesis

--- math_parser/math_parser/BET.h
+++ math_parser/math_parser/BET.h
@@ -6,6 +6,7 @@
 #include <string>
 #include <iostream>
 #include <cassert>
+#include <stack>
 
 enum class nodeType
 {
@@ -14,14 +15,6 @@ enum class nodeType
 	sign,
 	parent,
 	unknown
-};
-
-enum signType
-{
-	plus,
-	minus,
-	multiply,
-	devide
 };
 
 enum ParsingError
@@ -80,65 +73,10 @@ private:
 	std::string trimBrackets(std::string);
 	std::string setBrackets(node* parent_node, node* child_node);
 	node* openBrackets(node* p_node);
+	node* minusRefactor(node* p_node);
 	std::string toString(node* n);
 	ParsingError Analize(std::string expression);
 	void PrintParsingError(ParsingError e);
 
 	void destruct(node* p_node);
 };
-
-/*
-#include <cassert>
-#include <map>
-#include "node.h"
-
-typedef nodeType tokenType;
-
-enum ParsingError
-{
-	Ok,
-	Parantheses,
-	Identificator,
-	Signs
-};
-
-class BET
-{
-public:
-	BET();
-	BET(const char* expression);
-	BET(std::string expression);
-	~BET();
-
-	node* Parse(std::string expression);
-	std::string toString();
-	void openBrackets();
-	
-
-	class iterator
-	{
-	public:
-		std::string& operator*();
-		std::string& operator++();
-	private:
-	};
-
-private:
-	node* root;
-	std::string sourse;
-	std::map<std::string, node*> symboltable;
-
-	node* Parse(std::string expression, node* n);
-	std::string trimBrackets(std::string);
-	std::string setBrackets(node* parent_node, node* child_node);
-	node* openBrackets(node* p_node);
-	std::string toString(node* n);
-	ParsingError Analize(std::string expression);
-	void PrintParsingError(ParsingError e);
-
-	void destruct(node* p_node);
-};
-
-bool issign(const char c);
-
-*/
